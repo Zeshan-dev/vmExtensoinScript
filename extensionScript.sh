192e229dce7ad28f03c9efdef900357a7554e137
Zeshan Khalid <zeshan.khalid@neacllc.com>
2019-11-01 14:43:35 +0500

    create variables for input arguments

--- extensionScript.sh
+++ extensionScript.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+connString=${1}
+dbName=${2}
+
 sed -i '/COSMOS_CONN/d' /etc/profile
 sed -i '/COSMOS_DB/d' /etc/profile
 
-echo "export COSMOS_CONN=$1" >> /etc/profile
-echo "export COSMOS_DB=$2" >> /etc/profile
+echo "export COSMOS_CONN=$connString" >> /etc/profile
+echo "export COSMOS_DB=$dbName" >> /etc/profile
