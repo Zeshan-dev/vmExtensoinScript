4a94db5d805406eef30e843458c3b139b1218fa9
Zeshan Khalid <zeshan.khalid@neacllc.com>
2019-11-02 14:32:42 +0500

    Download required collections

--- extensionScript.sh
+++ extensionScript.sh
@@ -8,3 +8,17 @@ sed -i '/COSMOS_DB/d' /etc/profile
 
 echo "export COSMOS_CONN=$connString" >> /etc/profile
 echo "export COSMOS_DB=$dbName" >> /etc/profile
+
+# copy required collections to the database
+
+cd /numtraPlatform/mongoCollections
+
+curl https://raw.githubusercontent.com/Zeshan-dev/vmExtensoinScript/master/MongoCollections/components.json# >> components.json
+curl https://raw.githubusercontent.com/Zeshan-dev/vmExtensoinScript/master/MongoCollections/credentialprofiletypes.json# >> credentialprofiletypes.json
+curl https://raw.githubusercontent.com/Zeshan-dev/vmExtensoinScript/master/MongoCollections/resources.json# >> resources.json
+curl https://raw.githubusercontent.com/Zeshan-dev/vmExtensoinScript/master/MongoCollections/sections.json# >> sections.json
+
+mongoimport --uri=$connString --collection=credentialprofiletypes --drop --file=credentialprofiletypes.json
+mongoimport --uri=$connString --collection=components --drop --file=components.json
+mongoimport --uri=$connString --collection=resources --drop --file=resources.json
+mongoimport --uri=$connString --collection=sections --drop --file=sections.json
