b8353fe5c064cd78ff21875fe59149e545e5485f
Zeshan Khalid <zeshan.khalid@neacllc.com>
2019-11-22 17:46:50 +0500

    export mlflow workspace name

--- extensionScript.sh
+++ extensionScript.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-connString=${1}
-dbName=$(echo $connString | cut -d'/' -f 4 | cut -d'?' -f 1)
+mlflowWorkspaceName=${1}
+# dbName=$(echo $connString | cut -d'/' -f 4 | cut -d'?' -f 1)
 
 sed -i '/COSMOS_CONN/d' /etc/profile
 sed -i '/COSMOS_DB/d' /etc/profile
 
-echo "export COSMOS_CONN=$connString" >> /etc/profile
-echo "export COSMOS_DB=$dbName" >> /etc/profile
+echo "export mlflowWorkspaceName=$mlflowWorkspaceName" >> /etc/profile
+# echo "export COSMOS_DB=$dbName" >> /etc/profile
 
 # copy required collections to the database
 
