4d683b6618e3be88d37d98d7edd70cf4101aaba4
Zeshan Khalid <zeshan.khalid@neacllc.com>
2019-12-19 13:16:14 +0500

    update

--- extensionScript.sh
+++ extensionScript.sh
@@ -2,7 +2,7 @@
 
 mlflowWorkspaceName=${1}
 storageAccountUri=${2}
-storageAccountSas=${3}
+storageAccountSas="${3}"
 
 sed -i '/mlflowWorkspaceName/d' /etc/profile
 sed -i '/mlflow_workspace/d' /etc/profile
@@ -10,6 +10,7 @@ sed -i '/mongoStorageUri/d' /etc/profile
 sed -i '/mongoStorageSas/d' /etc/profile
 
 echo "export mlflow_workspace=$mlflowWorkspaceName" >> /etc/profile
+echo "export MLFLOWWORKSPACE=$mlflowWorkspaceName" >> /etc/profile
 echo "export mongoStorageUri=$storageAccountUri" >> /etc/profile
 echo "export mongoStorageSas=\"$storageAccountSas\"" >> /etc/profile
 
